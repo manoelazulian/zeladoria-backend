2cf56938ef03588c6ae92515d97420835711cec2
Manoela <manozulian@gmail.com>
2019-06-22 21:53:08 -0300

    add MoradiaRepository

---
+++ MoradiaRepository.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+// string de conexão:
+// mongodb://dominio:porta/nome_database
+const MONGO_URL = "mongodb://localhost:27017/zeladoria";
+
+function connect () {
+  mongoose.connect(MONGO_URL,
+    { useNewUrlParser: true },
+    function (error) {
+      if(error) {
+        console.error("Deu erro: ", error)
+      } else {
+        console.log("Conectamos no mongodb!!! o/")
+      }
+    }
+  );
+}
+
+module.exports = { connect }
+
