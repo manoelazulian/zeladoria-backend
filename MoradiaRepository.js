cd11b72b080beed33c2c3cd6057725d2552d870e
anarossini <anarossini01@gmail.com>
2019-07-15 17:35:25 -0300

    heroku mongo

--- MoradiaRepository.js
+++ MoradiaRepository.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URL = process.env.MONGODB_URI
-// const MONGO_URL = "mongodb://localhost:27017/zeladoria";
+const MONGO_URL = process.env.MONGODB_URI || "mongodb://localhost:27017/zeladoria"
 
 function connect () {
   mongoose.connect(MONGO_URL,
